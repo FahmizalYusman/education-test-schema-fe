ee70dfdc1b33af875afc49db418737ea48ccfdeb
FahmizalYusman <109127088+FahmizalYusman@users.noreply.github.com>
2022-07-12 15:00:19 +0700

    Add files via upload

---
+++ fahmizalyusman-test-fe-kawahedukasi.php
@@ -0,0 +1,50 @@
+<html>
+	<head>
+		<title>fahmizal yusman</title>
+	</head>
+	<body>
+		<h4>Data Pribadi :</h4>
+		<h4></h4>
+		<h4>Nama: Fahmizal Yusman</h4>
+		<h4>Alamat Email: fahmizalyusman23@gmail.com</h4>
+		<h4>waktu mulai: 19.30</h4>
+		<h4>waktu selesai: 21.00</h4>
+		<h4></h4>
+		<h4>1. Kami mempunyai data array dengan data :</h4>
+		<h4></h4>
+		<h4>var data1 = [1, 1, 1, 2, 2, 4, 1, 1 ]</h4>
+		<h4>var data2 = [2, 1, 2, 2, 2, 8, 1, 1 ]</h4>
+		<h4>var data3 = [3, 3, 3, 3, 2, 4, 1, 1 ]</h4>
+		<h4></h4>
+		<h4>var data = [2, 2, 2, 2, 4, 2, 1]</h4>
+		<h4>output : total data paling banyak keluar adalah 2 dengan jumlah 5</h4>
+		<h4></h4>
+		<h4></h4>
+		<h4>2. Kami mempunyai masalah pada validasi huruf dan angka, perusahaan kami membutuhkan validasi untuk pengecekan apakah inputan ini sesuai dengan keinginan kami</h4>
+		<h4>var input1 = "hallo jesika selamat datang!"</h4>
+		<h4>var input2 = "hallo anggun selamat datang!"</h4>
+		<h4>var input3 = "hallo ** selamat datang!"</h4>
+		<h4>var input4 = "hallo selamat datang!"</h4>
+		<h4></h4>
+		<h4>3. buatlah program looping untuk menampilkan output di bawah</h4>
+		<h4></h4>
+		<h4>output 1:</h4>
+		<h4></h4>
+		<h4>*</h4>
+		<h4>**</h4>
+		<h4>***</h4>
+		<h4>****</h4>
+		<h4>*****</h4>
+		<h4>******</h4>
+		<h4></h4>
+		<h4 align="center">output 2:</h4>
+		<h4></h4>
+		<h4 align="center">*</h4>
+		<h4 align="center">**</h4>
+		<h4 align="center">***</h4>
+		<h4 align="center">****</h4>
+		<h4 align="center">*****</h4>
+		<h4 align="center">******</h4>
+
+	</body>
+</html>
